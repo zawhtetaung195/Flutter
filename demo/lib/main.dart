b5d221e467750d8462413d2b1d5138012272e13b
zawhtetaung <zawhtetaung.tu009@gmail.com>
2022-06-26 08:25:42 +0630

    first commit

---
+++ demo/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        
+        appBar: AppBar(
+          title: Text ('MM Live Score'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      )
+    ),
+  );
+}
